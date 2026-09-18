e04d6220fd0ab55f7a74d63ff5f9c9eef2d136df
Gowthamidharmaraju <gowthamidharmaraju2000@gmail.com>
2026-09-18 10:29:59 +0530

    Fix Puppeteer launch args for reliable WhatsApp engine start on VPS

--- server/index.js
+++ server/index.js
@@ -15,17 +15,12 @@ global.waClient = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    executablePath: process.env.CHROME_BIN || undefined,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
       '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process',
       '--disable-gpu',
-      '--disable-extensions'
+      '--unhandled-rejections=strict'
     ]
   }
 });
