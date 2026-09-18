c71d7d61658e057baec2b27d373e82ccdcd1e71c
Gowthamidharmaraju <gowthamidharmaraju2000@gmail.com>
2026-09-18 15:31:05 +0530

    Revert "Remove Role Switcher dropdown and 'Switch to Dr. Tejaswini Ma\'am' buttons to prevent unauthorized role switching"
    This reverts commit 2a2b23f1da2a8377e0df3bfa247ca86dfd31e9a8.

--- public/js/views/contentDetail.js
+++ public/js/views/contentDetail.js
@@ -1188,6 +1188,9 @@ const ContentDetailView = {
             <div style="font-size: 0.72rem; color: var(--text-dim); margin-top: 4px;">
               Only Chief Editor Dr. Tejaswini Ma'am can approve this article to the next stage.
             </div>
+            <button class="btn btn-xs btn-outline-light mt-2" onclick="app.switchUser('usr-editor-1')">
+              Switch to Dr. Tejaswini Ma'am <i class="fa-solid fa-right-left"></i>
+            </button>
           </div>
         `;
       }
@@ -1221,6 +1224,9 @@ const ContentDetailView = {
             <div style="font-size: 0.8rem; font-weight: 700; color: var(--saffron-dark);">
               ⏳ Waiting for Final Approval (Dr. Tejaswini Ma'am Only)
             </div>
+            <button class="btn btn-xs btn-outline-light mt-2" onclick="app.switchUser('usr-editor-1')">
+              Switch to Dr. Tejaswini Ma'am <i class="fa-solid fa-right-left"></i>
+            </button>
           </div>
         `;
       }
